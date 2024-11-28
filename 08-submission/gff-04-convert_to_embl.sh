a26e1da29326e8942808bc5396c9be7ef3fa5134
Niko <nikolaos.papadopoulos@univie.ac.at>
2024-11-28 12:19:11 +0100

    update files to be used

--- 08-submission/gff-04-convert_to_embl.sh
+++ 08-submission/gff-04-convert_to_embl.sh
@@ -4,13 +4,12 @@ module load conda
 conda activate emblmygff3
 
 GENOME=/lisc/project/zoology/pycnogonum/paper/zenodo/results/draft.fasta
-GFF=/lisc/scratch/zoology/pycnogonum/genome/submission/merged_sorted_named_dedup_filtered.gff3
+GFF=/lisc/scratch/zoology/pycnogonum/genome/submission/merged_sorted_named_dedup_flagged.gff3
 RESDIR=/lisc/scratch/zoology/pycnogonum/genome/submission
 
 cd $RESDIR || exit
 
 EMBLmyGFF3 $GFF $GENOME \
-        --expose_translations \
         --topology linear \
         --molecule_type 'genomic DNA' \
         --transl_table 1  \
@@ -18,7 +17,7 @@ EMBLmyGFF3 $GFF $GENOME \
         --taxonomy INV \
         --locus_tag VPG \
         --project_id PRJEB80537 \
-        -vvv \
+        -v \
         -o result.embl
 
 gzip result.embl
